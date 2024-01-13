8e0edfeca13672c3fa7968a38a10bbf86e2e6c80
CR Ye <3342711246@qq.com>
2024-01-13 22:41:00 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MPI-MMD
 MPI multi-chip multi-core demo
+
+## Single chip single-core & multi-core
+
+```bash
+# single-core
+g++ -O2 ./single_chip.cpp && ./a.out
+
+# multi-core
+g++ -O2 -fopenmp ./single_chip.cpp && ./a.out
+```
+
+## Multi chip
+
+You must setup your ssh access and prepare host file (`my_hostfile`) before running the script.
+
+```bash
+./run.sh
+```
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mpicc -o multi_chip multi_chip.cpp
-scp multi_chip root@192.168.114.2:~/mpi_code
-scp multi_chip root@192.168.114.3:~/mpi_code
-scp multi_chip root@192.168.114.4:~/mpi_code
+scp multi_chip root@192.168.114.2:~/MPI-MMD
+scp multi_chip root@192.168.114.3:~/MPI-MMD
+scp multi_chip root@192.168.114.4:~/MPI-MMD
 mpirun -np 4 ./multi_chip
 mpirun -np 4 -hostfile my_hostfile ./multi_chip
